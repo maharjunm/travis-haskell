95de9e5bd1d173385d19755b83bd5894a39f0a17
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-08-02 01:03:04 +0530

    Add dummy events using Servant

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # EventUmbrellaServer
-Event Umbrella Server 
+Event Umbrella Server. Written in [Servant](http://haskell-servant.readthedocs.io/)
+
+## Setup
+
+`` sh
+# Install Haskell
+brew install haskell-stack
+
+# Clone the repository
+git clone git@github.com:twhyderabad/EventUmbrellaServer.git eu-server
+cd eu-server
+
+# Build & launch
+stack build
+stack exec eu-server-exe
+``
+
+After this [http://localhost:8080](http://localhost:8080) should be running!
---
+++ src/Lib.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+module Lib
+    ( startServer
+    ) where
+
+import Data.Text
+import Servant
+import Data.Aeson
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Data.Time.Calendar
+import GHC.Generics
+
+type Speaker = String
+
+data Event = Event
+  { title :: String
+  , speakers :: [Speaker]
+  , day :: Day
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Event
+
+geeknights :: [Event]
+geeknights =
+  [ Event "Artificial Intelligence and Machine Learning" ["Anup Vasudevan"] (fromGregorian 2016 08 3)
+  , Event "Evolution of Programming Languages" ["Jonnalagadda Srinivas"] (fromGregorian 2015 07 6)
+  ]
+
+type EventAPI = "events" :> Get '[JSON] [Event]
+
+server :: Server EventAPI
+server = return geeknights
+
+eventAPI :: Proxy EventAPI
+eventAPI = Proxy
+
+app :: Application
+app = serve eventAPI server
+
+startServer :: IO ()
+startServer= do
+  putStrLn "Running at http://localhost:8080"
+  run 8080 app

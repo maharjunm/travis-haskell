7637ce183f6c9c5bd734985375f743bff1f8d2d6
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-08-05 11:20:59 +0530

    Add PostgreSQL integration

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,55 +1,66 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Lib
-    ( startServer
-    ) where
+module Lib (startServer) where 
 
---import Data.Text
+import Data.Text (Text)
 import Data.List (isInfixOf)
 import Data.Char (toLower)
 import Servant
 import Data.Aeson
 import Network.Wai
-import Network.Wai.Handler.Warp
+import Network.Wai.Handler.Warp hiding (Connection)
 import Data.Time.Calendar
 import GHC.Generics
-
-type Speaker = String
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.ToField
+import Database.PostgreSQL.Simple.ToRow
+import Control.Applicative
+import Control.Monad.IO.Class (liftIO)
+import Data.Char (toLower)
 
 data Event = Event
-  { title :: String
-  , speakers :: [Speaker]
-  , day :: Day
+  { eventId :: Integer
+  , eventTitle :: Text
+  , eventSpeaker :: Text
+  , eventDate :: Day
   } deriving (Eq, Show, Generic)
 
 instance ToJSON Event
+instance FromJSON Event
 
-geeknights :: [Event]
-geeknights =
-  [ Event "Artificial Intelligence and Machine Learning" ["Anup Vasudevan"] (fromGregorian 2016 08 3)
-  , Event "Evolution of Programming Languages" ["Jonnalagadda Srinivas"] (fromGregorian 2015 07 6)
-  ]
+instance FromRow Event where
+  fromRow = Event <$> field <*> field <*> field <*> field
+
+instance ToRow Event where
+  toRow event = [ toField (eventId event)
+                , toField (eventTitle event)
+                , toField (eventSpeaker event)
+                , toField (eventDate event)]
 
 type EventAPI = "events" :> Get '[JSON] [Event]
   :<|> "event" :> Capture "id" Int :> Get '[JSON] Event
   :<|> "events" :> "search" :> Capture "name" String :> Get '[JSON] [Event]
 
-server :: Server EventAPI
-server = listEvents :<|> readEvent :<|> searchEvents
+server :: Connection -> Server EventAPI
+server conn = listEvents :<|> readEvent :<|> searchEvents
   where
-    listEvents = return geeknights
-    readEvent id = return (geeknights !! (id - 1))
-    searchEvents query = return (filter (\event -> (map toLower query) `isInfixOf` (map toLower (title event))) geeknights)
+    listEvents = liftIO $ query_ conn "SELECT * FROM events"
+    readEvent id = do
+      events <- liftIO $ query conn "SELECT * FROM events WHERE id = ?" (Only id)
+      return $ head events
+    searchEvents q = liftIO $ query conn "SELECT * FROM events WHERE LOWER(title) LIKE ?" (Only qu)
+      where qu :: String = "%" ++ (map toLower q) ++ "%"
 
 eventAPI :: Proxy EventAPI
 eventAPI = Proxy
 
-app :: Application
-app = serve eventAPI server
-
 startServer :: IO ()
 startServer= do
   putStrLn "Running at http://localhost:8080"
-  run 8080 app
+  conn <- connectPostgreSQL "host=localhost user=eu dbname=eu"
+  run 8080 . serve eventAPI $ server conn
